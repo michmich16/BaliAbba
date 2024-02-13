ce896d25d4a27a115e1a6946eaeab65b0a38697b
Michael P <michpouls@gmail.com>
2024-02-13 13:10:10 +0100

    #8 delvist skrev

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,51 @@
-fetchCategoryData()
+//fetchCategoryData()
 fetchProductData()
+receivedProductData()
 
-function fetchCategoryData(){
-    fetch('https://dummyjson.com/products/categories')
-    .then(response => response.json())
-    .then(json => console.log(json));
+ function fetchProductData() {
+         fetch('https://dummyjson.com/products')
+         .then((result) => {
 
-    //receivedCategoryData(json)
+            return result.json();
+        })
+        .then((json) => {
+
+            receivedProductData(json);
+
+        });
+
+    }
+
+function receivedProductData(ProductData) {
+    //console.log(ProductData);
+
+    let myProducts = ProductData.products
+
+    let myFeaturedProducts = [];
+
+    myFeaturedProducts.push(myProducts[6], myProducts[9], myProducts[23])
+
+    console.log(myFeaturedProducts);
 
 }
 
-function fetchProductData(){
-    fetch('https://dummyjson.com/products')
-    .then(response => response.json())
-    .then(json => console.log(json));
 
-    //receivedProductData(json)
-}
+
+
+
+
+
+
+    // function fetchCategoryData() {
+    //     fetch('https://dummyjson.com/products/categories')
+    //     .then((result) => {
+    
+    //         return result.json();
+    //     })
+    //     .then((json) => {
+    
+    //         receivedCategoryData(json);
+    
+    //     });
+    
+    // }
